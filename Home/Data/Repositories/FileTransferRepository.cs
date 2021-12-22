d94a8b024c8d54ecb48b7d5a6a38deb75f0bdb36
mcast-LukeAttard <luke.attard.c10035@mcast.edu.mt>
2021-12-22 19:51:51 +0100

    Created FileTransferRepository & IFileTransferRepository

---
+++ Home/Data/Repositories/FileTransferRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Repositories
+{
+    public class FileTransferRepository : IFileTransferRepository
+    {
+
+    }
+}
