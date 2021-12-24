d3b3f541faf9801538048931c2a2520c241660aa
mcast-LukeAttard <luke.attard.c10035@mcast.edu.mt>
2021-12-24 01:12:03 +0100

    Created IFilesService and FilesService

---
+++ Home/Application/Services/FilesService.cs
@@ -0,0 +1,18 @@
+﻿using Application.Interfaces;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Services
+{
+    public class FilesService : IFilesService
+    {
+        private IFilesTransferRepository _filesTransferRepository;
+        public FilesService(IFilesTransferRepository _ftRepo)
+        {
+            _filesTransferRepository = _ftRepo;
+        }
+
+    }
+}
