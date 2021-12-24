316a6130116dc3591aaf343796955f3e15d02493
mcast-LukeAttard <luke.attard.c10035@mcast.edu.mt>
2021-12-24 01:23:43 +0100

    FilesController Constructor

--- Home/Presentation/Controllers/FilesController.cs
+++ Home/Presentation/Controllers/FilesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
@@ -6,9 +8,12 @@ namespace Presentation.Controllers
     [Authorize]
     public class FilesController : Controller
     {
-        public FilesController()
+        private IFilesService filesService;
+        private IWebHostEnvironment webHostEnvironment;
+        public FilesController(IFilesService _filesService, IWebHostEnvironment _webHostEnvironment)
         {
-
+            filesService = _filesService;
+            webHostEnvironment = _webHostEnvironment;
         }
         public IActionResult Index()
         {
